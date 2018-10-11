35ef6a87b6d7d93ff46b80c010f6c1b1ba3075c7
McPrapor <max.terleckiy@gmail.com>
2018-10-11 09:24:19 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -69,8 +69,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 # Common stuff
 $(call inherit-product, vendor/mad/config/common.mk)
 
-# Fix wifi?
-include vendor/mad/config/wifi.mk
+include $(LOCAL_PATH)/product/*.mk
 
 # Vendor
 $(call inherit-product, vendor/coolpad/note3/note3-vendor.mk)
