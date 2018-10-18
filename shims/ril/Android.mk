054f9040895926e93b984a6f972420e6568e2186
McPrapor <max.terleckiy@gmail.com>
2018-10-18 15:54:25 +0200

    Create Android.mk

---
+++ shims/ril/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := mtk-ril_shim.c
+LOCAL_MULTILIB := both
+LOCAL_MODULE := libmtk-ril_shim
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES += system/core/liblog/include/
+LOCAL_SHARED_LIBRARIES := liblog
+include $(BUILD_SHARED_LIBRARY)
