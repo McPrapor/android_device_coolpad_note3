d8c9fd32a1c0da24a79adaf28bc3f782820f3fb4
McPrapor <max.terleckiy@gmail.com>
2018-10-24 10:16:26 +0200

    Update mtk-ril_shim.c

--- shims/ril/mtk-ril_shim.c
+++ shims/ril/mtk-ril_shim.c
@@ -1,33 +1,46 @@
 #include <log/log.h>
 #include <cutils/log.h>
 
+#define SHIMDEBUG 1
+
 void (*real_switchStkUtkModeByCardType)(double rid) = NULL;
 static void (*real_handleCardTypeUrc)(const char *s, RIL_SOCKET_ID rid) = NULL;
 
 void __attribute__((constructor)) initialize(void) {
   real_switchStkUtkModeByCardType = dlsym(RTLD_NEXT, "switchStkUtkModeByCardType");
   if (real_switchStkUtkModeByCardType == NULL) {
-    RLOGD("real_switchStkUtkModeByCardType not found");
+#ifdef SHIMDEBUG
+    RLOGD("SHIM real_switchStkUtkModeByCardType not found");
+#endif
   }
   real_fopen = dlsym(RTLD_NEXT, "handleCardTypeUrc");
   if (real_handleCardTypeUrc == NULL) {
-    RLOGD("real_handleCardTypeUrc not found");
+#ifdef SHIMDEBUG    
+    RLOGD("SHIM real_handleCardTypeUrc not found");
+#endif    
   }
 }
 
 void switchStkUtkModeByCardType(double rid) {
-    RLOGD("switchStkUtkModeByCardType SHIM call rid: %d", rid);
+#ifdef SHIMDEBUG      
+    RLOGD("SHIM switchStkUtkModeByCardType call rid: %d", rid);
+#endif
     if (real_switchStkUtkModeByCardType == NULL) {
-      RLOGD("real_switchStkUtkModeByCardType found, calling...");
+#ifdef SHIMDEBUG          
+      RLOGD("SHIM real_switchStkUtkModeByCardType found, calling...");
+#endif      
       return real_switchStkUtkModeByCardType(rid);
     }
 }
 
-
 static void handleCardTypeUrc(const char *s, RIL_SOCKET_ID rid) {
-    RLOGD("handleCardTypeUrc SHIM call rid: %d", rid);
+#ifdef SHIMDEBUG    
+    RLOGD("SHIM handleCardTypeUrc call rid: %d", rid);
+#endif
     if (real_handleCardTypeUrc != NULL) {
-        RLOGD("real_handleCardTypeUrc found, calling...");
+#ifdef SHIMDEBUG          
+        RLOGD("SHIM real_handleCardTypeUrc found, calling...");
+#endif      
         return real_handleCardTypeUrc(s, rid);
     }
 }
