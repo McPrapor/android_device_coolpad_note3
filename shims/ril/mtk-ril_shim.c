7af7816c7bf54b7f4bc7740c72d0ae57a4517b4a
McPrapor <max.terleckiy@gmail.com>
2018-10-24 08:52:26 +0200

    Update mtk-ril_shim.c

--- shims/ril/mtk-ril_shim.c
+++ shims/ril/mtk-ril_shim.c
@@ -4,3 +4,8 @@
 void switchStkUtkModeByCardType(double rid) {
     RLOGD("switchStkUtkModeByCardType SHIM call rid: %d", rid);
 }
+
+
+static void handleCardTypeUrc(const char *s, RIL_SOCKET_ID rid) {
+    RLOGD("handleCardTypeUrc SHIM call rid: %d", rid);
+}
