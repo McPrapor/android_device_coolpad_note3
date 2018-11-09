0e418897e83c34a05a7f9b500ecc0ee896b1f56e
McPrapor <max.terleckiy@gmail.com>
2018-11-09 14:04:36 +0100

    Delete Android.mk

--- shims/catomic/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := atomic_shim.c
-LOCAL_MULTILIB := both
-LOCAL_MODULE := libatomic_shim
-LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := libcutils
-include $(BUILD_SHARED_LIBRARY)
