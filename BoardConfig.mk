a6bd946c198f62d313e91dd0892580fa5623d81f
McPrapor <max.terleckiy@gmail.com>
2018-11-09 12:39:50 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,17 +202,12 @@ PRODUCT_PACKAGES += \
     libmtkshim_camera \
     libmtkshim_gps \
     libhtcxlog_shim
-#    \
-#    libshim_atomic
 
-#   libatomic_shim
-#    /system/lib/libcutils.so|libatomic_shim.so
-#    /system/lib64/libcutils.so|libatomic_shim.so
 
 TARGET_LD_SHIM_LIBS := \
     $(TARGET_LD_SHIM_LIBS) \
-#    /system/lib/libcutils.so|libshim_atomic.so \
-#    /system/lib64/libcutils.so|libshim_atomic.so \    
+    /system/lib/libcutils.so|libshim_atomic.so \
+    /system/lib64/libcutils.so|libshim_atomic.so \    
     /system/lib/liblog.so|libhtcxlog_shim.so \
     /system/lib64/liblog.so|libhtcxlog_shim.so \
     /system/vendor/lib/hw/audio.primary.$(TARGET_BOARD_PLATFORM).so|libmtkshim_audio.so \
