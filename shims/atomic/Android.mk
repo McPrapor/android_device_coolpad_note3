3fc28067382e1889d28384aef0ce4fd26b646efa
McPrapor <max.terleckiy@gmail.com>
2018-11-09 13:49:59 +0100

    Update Android.mk

--- shims/atomic/Android.mk
+++ shims/atomic/Android.mk
@@ -1,10 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
 
+# libcutils
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := atomic.c
-#LOCAL_WHOLE_STATIC_LIBRARIES := libcutils
+LOCAL_WHOLE_STATIC_LIBRARIES := libcutils
 LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_MODULE := libcutils_shim
+LOCAL_MODULE := libc_util
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
