c85cb23aff5b951bb20f1e1423ba826856ca1e80
McPrapor <max.terleckiy@gmail.com>
2018-11-09 14:48:45 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -193,11 +193,10 @@ TARGET_INCLUDE_CAMERA_SYMBOLS := true
 # Include symbols
 # Shim libraries
 # Add to PRODUCT_PACKAGES at device.mk
-
+#    /system/lib/libcutils.so|libmtkshim_atomic.so \
+#    /system/lib64/libcutils.so|libmtkshim_atomic.so \
 TARGET_LD_SHIM_LIBS := \
     $(TARGET_LD_SHIM_LIBS) \
-    /system/lib/libcutils.so|libmtkshim_atomic.so \
-    /system/lib64/libcutils.so|libmtkshim_atomic.so \
     /system/lib/liblog.so|libhtcxlog_shim.so \
     /system/lib64/liblog.so|libhtcxlog_shim.so \
     /system/vendor/lib/hw/audio.primary.$(TARGET_BOARD_PLATFORM).so|libmtkshim_audio.so \
