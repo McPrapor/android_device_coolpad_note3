9965e8bb899569bbfc9d750fe7379420683a100e
McPrapor <max.terleckiy@gmail.com>
2018-11-09 13:37:54 +0100

    Update Android.mk

--- shims/atomic/Android.mk
+++ shims/atomic/Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := atomic.cpp
-LOCAL_MULTILIB := both
 LOCAL_MODULE := libshim_atomic
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_CFLAGS := -O3 -Wno-unused-variable -Wno-unused-parameter
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
