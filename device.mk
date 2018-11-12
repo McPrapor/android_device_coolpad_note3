1eb7471e81d49cab83ad7e7736d7c1e73d36b131
McPrapor <max.terleckiy@gmail.com>
2018-11-12 11:25:27 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -317,6 +317,18 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
 
+# Google properties overides
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dataroaming=false \
+    net.tethering.noprovisioning=true
+
 # Vendor
 $(call inherit-product, vendor/coolpad/note3/note3-vendor.mk)
 $(call inherit-product, vendor/coolpad/note3/note3-firmware-blobs.mk)
