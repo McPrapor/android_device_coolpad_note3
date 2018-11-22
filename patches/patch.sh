d1919e7db8f3d17af8ac53cfd804c6dfb13f7fc4
McPrapor <max.terleckiy@gmail.com>
2018-11-22 11:50:32 +0100

    Create patch.sh

---
+++ patches/patch.sh
@@ -0,0 +1,17 @@
+echo $1
+rootdirectory="$PWD"
+# ---------------------------------
+
+dirs="sensors"
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo "Applying $dir patches..."
+	git apply $rootdirectory/device/coolpad/note3/patches/$dir/*.patch
+	echo " "
+done
+
+# -----------------------------------
+echo "Changing to build directory..."
+cd $rootdirectory
