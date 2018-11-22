ef75975c1448dc5dd98c1af7e04f61201373b43f
McPrapor <max.terleckiy@gmail.com>
2018-11-22 12:10:04 +0100

    Update unpatch.sh

--- patches/unpatch.sh
+++ patches/unpatch.sh
@@ -2,15 +2,18 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="sensors"
+dirs="hardware/interfaces "
 
 for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
 	echo "Reverting $dir patches..."
-	git apply --reverse device/coolpad/note3/patches/$dir/*.patch
-	#git reset --hard
+	#git apply --reverse $rootdirectory/device/coolpad/note3/patches/$dir/*.patch
+	git reset --hard
 	git clean -f -d
 	echo " "
 done
 
 # -----------------------------------
 echo "Changing to build directory..."
+cd $rootdirectory
