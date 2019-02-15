b49a8a98b5392e358aa710a14671ecefde1152d4
McPrapor <max.terleckiy@gmail.com>
2019-02-15 14:14:47 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -330,10 +330,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
 
 # HTC build props
-PRODUCT_PROPERTY_OVERRIDES += \
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.com.google.clientidbase=android-htc \
+#    ro.build.fingerprint=htc/v36bml_dugl_00401/htc_v36bml_dugl:5.1/LMY47D/822405.2:user/release-keys \
+#    ro.build.buildline=V36BMLDUGL_HTC_WWE_L51_CRC_DesireSense70_Stable
+    
+PRODUCT_BUILD_PROP_OVERRIDES += \
     ro.com.google.clientidbase=android-htc \
     ro.build.fingerprint=htc/v36bml_dugl_00401/htc_v36bml_dugl:5.1/LMY47D/822405.2:user/release-keys \
-    ro.build.buildline=V36BMLDUGL_HTC_WWE_L51_CRC_DesireSense70_Stable
+    ro.build.buildline=V36BMLDUGL_HTC_WWE_L51_CRC_DesireSense70_Stable \
+    ro.build.description="1.17.401.2" "CL822405" "release-keys"
 
 # Some more apps
 #PRODUCT_PACKAGES += \
