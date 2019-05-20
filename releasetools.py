d5b05cd32183e1abf37e4cb4da2a8ba0de1325b8
McPrapor <max.terleckiy@gmail.com>
2019-05-20 09:41:29 +0200

    Update releasetools.py

--- releasetools.py
+++ releasetools.py
@@ -6,9 +6,9 @@ def FixPaths(info):
     for i in xrange(len(edify.script)):
       if "ui_print(" in edify.script[i] and "By:-" in edify.script[i]:
         edify.script[i] = 'ui_print("*                   By:-m4h0n3y                  *");'
-    for i in xrange(len(edify.script)):
-      if "mtk-msdc.0/11230000.msdc0" in edify.script[i]:
-        edify.script[i] = edify.script[i].replace('mtk-msdc.0/11230000.msdc0','mtk-msdc.0')
+#    for i in xrange(len(edify.script)):
+#      if "mtk-msdc.0/11230000.msdc0" in edify.script[i]:
+#        edify.script[i] = edify.script[i].replace('mtk-msdc.0/11230000.msdc0','mtk-msdc.0')
     return
 
 def FullOTA_InstallEnd(info):
